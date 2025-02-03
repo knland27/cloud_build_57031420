42120811cc6891051da87959fb44445d016167c5
knland27 <kevin.torres@telsal.cloud>
2025-02-03 21:35:04 +0000

    applying changes to main.py

--- src/main.py
+++ src/main.py
@@ -7,5 +7,9 @@ port = 8080
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/create-post')
+def create_post():
+    return 'Post was created...'
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port, threaded=True)
